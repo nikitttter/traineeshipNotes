46db5451ff0f0094479e7e3c6fecb5cb37bfdf09
Nikita Yakovlev <Nikitttter@gmail.com>
2022-03-27 17:43:06 +0300

    initial commit for HW1

---
+++ Notes/Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Nikita Yakovlev on 27.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
